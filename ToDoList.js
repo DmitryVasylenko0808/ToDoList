b46c8d97b4dc466804b373480c1ad6cb25180537
Dmitry <dvasilenko1999@gmail.com>
2022-02-22 09:53:13 +0200

    main

--- ToDoList.js
+++ ToDoList.js
@@ -1,88 +1,117 @@
 class ListTodo extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        item: {
-          id: 0,
-          title: "",
-          isDone: false
-        },
-        toDoItems: []
-      }
-      
-      this.clickAdd = this.clickAdd.bind(this);
-      this.handleChange = this.handleChange.bind(this);
-    }
-    
-    handleChange(e) {
-      const item = {
-        id: this.state.item.id,
-        title: e.target.value,
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: {
+        id: 0,
+        title: "",
         isDone: false
-      }
-      this.setState({item: item})
+      },
+      toDoItems: []
     }
     
-    clickAdd() {
-      const toDoItems = this.state.toDoItems.slice();
-      const itemAdd = {
-        id: this.state.item.id + 1,
-        title: this.state.item.title,
-        isDone: false
-      };
-     
-      toDoItems.push(itemAdd);
-      
-      this.setState({
-        item: itemAdd,
-        toDoItems: toDoItems})
-    }
-    
-    clickDelete(i) {
-      const toDoItems = this.state.toDoItems.slice();
-      toDoItems.splice(i, 1);
-      this.setState({toDoItems: toDoItems});
-    }
-    
-    renderToDoItems() {
-      const arrayItemComponents = [];
-      for(let i = 0; i < this.state.toDoItems.length; i++) {
-        let itemComponent = <ToDoItem 
-                              key={i.toString()}
-                              value={this.state.toDoItems[i]}
-                              onClick={() => this.clickDelete(i)}
-                              />;
-        arrayItemComponents.push(itemComponent);
-      }
-      return <ul>{arrayItemComponents}</ul>;
-    }
-    
-    render() {
-      return (
-         <div>
-          <input type="text" onChange={this.handleChange}/>
-          <button onClick={this.clickAdd}>Add</button>
-          {this.renderToDoItems()}
-        </div>)
-    }
+    this.clickAdd = this.clickAdd.bind(this);
+    this.handleChange = this.handleChange.bind(this);
   }
   
-  function ToDoItem(props) {
-    let data = {
-      id: props.value.id,
-      title: props.value.title,
-      isDone: props.value.isDone
+  handleChange(e) {
+    const item = {
+      id: this.state.item.id,
+      title: e.target.value,
+      isDone: false
     }
+    this.setState({item: item})
+  }
+  
+  clickAdd() {
+    const toDoItems = this.state.toDoItems.slice();
+    const itemAdd = {
+      id: this.state.item.id + 1,
+      title: this.state.item.title,
+      isDone: false
+    };
+   
+    toDoItems.push(itemAdd);
     
+    this.setState({
+      item: itemAdd,
+      toDoItems: toDoItems})
+  }
+  
+  clickDelete(i) {
+    const toDoItems = this.state.toDoItems.slice();
+    toDoItems.splice(i, 1);
+    this.setState({toDoItems: toDoItems});
+  }
+  
+  renderToDoItems() {
+    const arrayItemComponents = [];
+    for(let i = 0; i < this.state.toDoItems.length; i++) {
+      let itemComponent = <ToDoItem 
+                            key={i.toString()}
+                            value={this.state.toDoItems[i]}
+                            onClick={() => this.clickDelete(i)}
+                            class="input-data"
+                            />;
+      arrayItemComponents.push(itemComponent);
+    }
+    return <ul>{arrayItemComponents}</ul>;
+  }
+  
+  render() {
     return (
-      <li>
-        <div>
-          <p>Item{data.id}</p>
-          <input type="text" value={data.title}/>
-          <input type="checkbox" checked={data.isDone} />
-          <button onClick={props.onClick}>Delete</button>
-        </div>
-      </li>)
+       <div className="container">
+        <h1>ToDo List</h1>
+        <input className="input-data" type="text" onChange={this.handleChange}/>
+        <Button value={true} onClick={this.clickAdd} />
+        <h2>List:</h2>
+        {this.renderToDoItems()}
+      </div>)
+  }
+}
+
+function ToDoItem(props) {
+  let data = {
+    id: props.value.id,
+    title: props.value.title,
+    isDone: props.value.isDone
   }
   
-  ReactDOM.render(<ListTodo />, document.getElementById('root'));
+  return (
+    <li>
+      <div className="item">
+        <input class={props.class} type="text" value={data.title}/>
+        <input type="checkbox" defaultChecked={data.isDone} id={"checkbox" + data.id}/>
+        <label for={"checkbox" + data.id}></label>
+        <Button value={false} onClick={props.onClick}/>
+      </div>
+    </li>)
+}
+
+function ButtonDel(props) {
+  return (<button onClick={props.onClick}>
+           Delete
+         </button>);
+}
+
+function ButtonAdd(props) {
+  return (<button onClick={props.onClick}>
+    Add
+  </button>);
+}
+
+function Button(props) {
+  const value = props.value;
+  
+  if(value) {
+    return <ButtonAdd 
+             onClick={props.onClick}/>
+  }
+  else {
+    return <ButtonDel 
+             onClick={props.onClick}
+             />
+  }
+}
+
+ReactDOM.render(<ListTodo />, document.getElementById('root'));
